96f40d83dada8e432c2fca3fee7aa23e3229b0c8
Zahid Patoary <zahidpatoary@Zahids-MacBook-Air.local>
2022-07-31 00:54:48 +0900

    adding steps

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const query = require('express/lib/middleware/query');
+const { decode } = require('jsonwebtoken');
 
 require('dotenv').config();
 const port = process.env.PORT || 4000;
@@ -13,55 +15,104 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message:'unauthorized access'})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decode) =>{
+        if(err){
+            return res.status(403).send({message:'Forbidden access'})
+        }
+        console.log('decoded', decode);
+        req.decode = decode;
+        next();
+    })
+
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2kgpg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const serviceCollection = client.db('carServices').collection('service');
-        
-        app.get('/service',async(req, res) =>{
+        const orderCollection = client.db('carServices').collection('order');
+        // AUTH
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
+
+        // service api
+        app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         });
 
-       app.get('/service/:id', async(req, res) =>{
-           const id = req.params.id;
-           const query = {_id:ObjectId(id)};
-           const service = await serviceCollection.findOne(query);
-           res.send(service);
-       });
-       //post
-       app.post('/service', async(req, res)=>{
-           const newService = req.body;
-           const result = await serviceCollection.insertOne(newService);
-           res.send(result);
-       });
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+        //post (create kora)
+        app.post('/service', async (req, res) => {
+            const newService = req.body;
+            const result = await serviceCollection.insertOne(newService);
+            res.send(result);
+        });
 
-       //Delete
-       app.delete('/service/:id', async(req, res) =>{
-           const id = req.params.id;
-           const query = {_id:ObjectId(id)};
-           const result = await serviceCollection.deleteOne(query);
-           res.send(result);
-       })
+        //Delete
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
+        // Order Collection API
+        app.get('/order', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decode.email;
+            const email = req.query.email;                 // kon user ar koita order ta ber korte 
+         if(email === decodedEmail){
+            const query = { email: email };                   // email diya query baniya filter kora
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+         }else{
+             res.status(403).send({message: 'forbidden access'})
+         }
+
+        })
+
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        })
 
     }
-    finally{
+    finally {
 
     }
 }
 
 run().catch(console.dir);
 
-app.get('/',(req,res) =>{
+app.get('/', (req, res) => {
     res.send('Running Car Server')
 })
 
-app.listen(port, ()=>{
-    console.log('Listening to pore',port);
+app.listen(port, () => {
+    console.log('Listening to pore', port);
 });
